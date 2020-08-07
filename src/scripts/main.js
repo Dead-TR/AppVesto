85c666e434ce838986aeb4b0ba12f486f900d947
DTR <t.terry.rice@gmail.com>
2020-08-07 17:44:05 +0300

    mobile end

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,17 +23,8 @@ skillCheckers.addEventListener('click', (event) => {
 const dropDescription = document.querySelector('.description');
 
 dropDescription.addEventListener('click', (event) => {
-
-  let activeDrop;
-  let arrow;
-
-  if (event.target.parentElement.classList.contains('description__item')) {
-    activeDrop = event.target.parentElement;
-    arrow = event.target.previousElementSibling;
-  } else if (event.target.classList.contains('description__item')) {
-    activeDrop = event.target;
-    arrow = activeDrop.firstElementChild;
-  };
+  const activeDrop = event.target.closest('.description__item');
+  const arrow = activeDrop.firstElementChild;
 
   if (activeDrop) {
     if (activeDrop.classList.contains('description__item_closed')) {
@@ -107,6 +98,69 @@ dropListParent.addEventListener('click', (event) => { // make choice listener
     dropListParent.className = dropLouver(dropdownSelectElement.dataset.open);
     dropdownSelectShow(event.target.dataset.dropdownvalue);
   }
+});
+
+// Filter
+let amountState = 0;
+const filter = document.querySelector('.mobile-filter');
+
+filter.addEventListener('click', (event)=> {
+
+  if (event.target.className === 'mobile-filter__img') { // open filter list
+    const filterList = document.querySelector('.mobile-filter__list');
+
+    filterList.classList.toggle('mobile-filter__list_open');
+  }
+
+  if (event.target.closest('.mobile-filter__item')) { // checked/unchecked
+    const amountItem = document.querySelector('.mobile-filter__amount');
+    const filterItem = event.target.closest('.mobile-filter__item');
+    const filterItemChild = [...filterItem.children];
+
+    filterItemChild[0].checked = (!filterItemChild[0].checked);
+
+    if (filterItemChild[0].checked) {
+      filterItemChild[1].classList.add('mobile-filter__replace_check');
+      filterItemChild[1].innerText = '✓';
+      amountItem.innerText = `${++amountState}`;
+    } else {
+      filterItemChild[1].classList.remove('mobile-filter__replace_check');
+      filterItemChild[1].innerText = '';
+      amountItem.innerText = `${--amountState} `;
+    }
+
+    if (amountState > 0) {
+      amountItem.classList.add('mobile-filter__amount_show');
+    } else {
+      amountItem.classList.remove('mobile-filter__amount_show');
+    }
+  }
+
+  if (event.target.classList.contains('mobile-filter__amount')) { // unchecked all
+    const filterList = [
+      ...document.querySelector('.mobile-filter__list').children,
+    ];
+
+    for (const listItem of filterList) {
+      const filterItemChild = [...listItem.children];
+
+      filterItemChild[0].checked = false;
+      filterItemChild[1].innerText = '';
+      filterItemChild[1].classList.remove('mobile-filter__replace_check');
+      amountState = 0;
+      event.target.classList.remove('mobile-filter__amount_show');
+    }
+  }
+});
+
+// mobile main menu:
+const buttonSlideMenu = document.querySelector('.header__button-side-menu');
+
+buttonSlideMenu.addEventListener('click', () => {
+  const mobileNav = document.querySelector('.mobile-content');
+
+  buttonSlideMenu.classList.toggle('header__button-side-menu_flipped');
+  mobileNav.classList.toggle('mobile-content_hide');
 });
 
 // General functions:
