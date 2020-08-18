24c6721c064f4408ef6a33bb9e295d570ac9de21
DTR <t.terry.rice@gmail.com>
2020-08-18 16:31:13 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,14 @@ const formInput = document.querySelector('.join__form').firstElementChild;
 if (document.body.offsetWidth <= 360) {
   formInput.placeholder = 'Your email';
 }
+
+// mobile slide-menu
+
+const buttonSlideMenu = document.querySelector('.header__button-side-menu');
+
+buttonSlideMenu.addEventListener('click', () => {
+  const mobileNav = document.querySelector('.mobile-content');
+
+  buttonSlideMenu.classList.toggle('header__button-side-menu_flipped');
+  mobileNav.classList.toggle('mobile-content_hide');
+});
